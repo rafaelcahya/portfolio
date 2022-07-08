a5e5d4d1704d8763750ba615c638db7bb5493d24
rafaelcahya <cahyaputraugira95@gmail.com>
2022-07-08 14:39:25 +0700

    Update

--- src/pages/project/SingleProject.jsx
+++ src/pages/project/SingleProject.jsx
@@ -1,3 +1,4 @@
+import { marked } from 'marked';
 import React, { Component } from 'react';
 import client from '../../client';
 
@@ -36,44 +37,17 @@ export default class SingleProject extends Component {
                             </div>
                         </div>
                         <div className='flex flex-col gap-96 py-32 px-5'>
-                            <div className='flex flex-col items-center text-center gap-10'>
-                                <img src={"https:" + this.state.article.fields.featureImage1.fields.file.url} alt="" data-aos='zoom-in' data-aos-easing="ease-out" data-aos-delay="200" data-aos-duration="500"/>
-                                <p className='jakarta-700 text-[25px]' data-aos='fade-up' data-aos-easing="ease-out" data-aos-duration="500">{this.state.article.fields.featureName1}</p>
-                                <p className='text-[18px] lg:text-[22px] w-full lg:w-3/4 leading-9' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500">{this.state.article.fields.featureDescription1}</p>
+                            <div className='flex flex-col gap-10 mx-10 md:mx-32 xl:mx-64'>
+                                <p className='jakarta-700 text-[25px]' data-aos='fade-up' data-aos-easing="ease-out" data-aos-duration="500">{this.state.article.fields.title}</p>
+                                <p className='text-[18px] lg:text-[22px] w-full lg:w-4/5 leading-9' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500">{this.state.article.fields.description}</p>
                             </div>
-                            {
-                                !this.state.article.fields.featureImage2 &&
-                                !this.state.article.fields.featureName2 &&
-                                !this.state.article.fields.featureDescription2 ? (null) : (
-                                    <div className='flex justify-center'>
-                                        <div className='flex flex-col lg:flex-row items-center gap-14 lg:gap-20 1-full lg:w-3/4 xl:w-[1000px]'>
-                                            <img src={"https:" + this.state.article.fields.featureImage2.fields.file.url} alt=""data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="200" data-aos-duration="500"/>
-                                            <div className='flex flex-col text-center lg:text-left gap-10 lg:gap-5'>
-                                                <p className='jakarta-700 text-[25px]'data-aos='fade-up' data-aos-easing="ease-out" data-aos-duration="500">{this.state.article.fields.featureName2}</p>
-                                                <p className='text-[18px] lg:text-[22px] leading-9' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500">{this.state.article.fields.featureDescription2}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                            {
-                                !this.state.article.fields.featureImage3 &&
-                                !this.state.article.fields.featureName3 &&
-                                !this.state.article.fields.featureDescription3 ? (null):(
-                                    <div className='flex justify-center'>
-                                        <div className='flex flex-col lg:flex-row-reverse items-center gap-14 lg:gap-20 1-full lg:w-3/4 xl:w-[1000px]'>
-                                            <img src={"https:" + this.state.article.fields.featureImage3.fields.file.url} alt=""data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="200" data-aos-duration="500"/>
-                                            <div className='flex flex-col text-center lg:text-left gap-10 lg:gap-5'>
-                                                <p className='jakarta-700 text-[25px]'data-aos='fade-up' data-aos-easing="ease-out" data-aos-duration="500">{this.state.article.fields.featureName3}</p>
-                                                <p className='text-[18px] lg:text-[22px] leading-9' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500">{this.state.article.fields.featureDescription3}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            }
+                            <div className='flex flex-col gap-10 mx-10 md:mx-32 xl:mx-64'>
+                                <p className='jakarta-700 text-[25px]' data-aos='fade-up' data-aos-easing="ease-out" data-aos-duration="500">The challenge</p>
+                                <p className='text-[18px] lg:text-[22px] w-full lg:w-4/5 leading-9' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500">{this.state.article.fields.anotherFeature}</p>
+                            </div>
                             <div className='flex flex-col items-center gap-20'>
                                 <p className='jakarta-700 text-[25px]'data-aos='fade-up' data-aos-easing="ease-out" data-aos-duration="500">Full design</p>
-                                <img src={"https:" + this.state.article.fields.fullDesignImage.fields.file.url} alt="" className='w-full lg:w-3/4' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500"/>
+                                <img src={"https:" + this.state.article.fields.displayImage.fields.file.url} alt="" className='w-full lg:w-3/4' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500"/>
                             </div>
                         </div>
                     </div>
@@ -82,41 +56,3 @@ export default class SingleProject extends Component {
         )
     }
 }
-
-//     <div className='flex h-screen'>
-//                                     <div className='m-auto px-32'>
-//                                         <p className='text-[#8A8A8A] pb-2'>Description</p>
-//                                         <span className='underline-animation bg-gradient-to-r from-cyan-500 to-blue-500 text-[25px] tracking-wider leading-[50px]'>{this.state.article.fields.longDescription}</span>
-//                                     </div>
-//                                 </div>
-//                                 <div className='flex h-screen'>
-//                                     <div className='flex justify-between items-center px-32 w-3/4'>
-//                                         <div className='flex flex-col gap-2'>
-//                                             <p className='text-[#8A8A8A]'>Tech Stack</p>
-//                                             <p className='underline-animation bg-gradient-to-r from-cyan-500 to-blue-500 text-[25px] tracking-wider'>{this.state.article.fields.techStack}</p>
-//                                         </div>
-//                                         <div className='flex flex-col gap-2'>
-//                                             <p className='text-[#8A8A8A]'>Project Length</p>
-//                                             <p className='underline-animation bg-gradient-to-r from-cyan-500 to-blue-500 text-[25px] tracking-wider'>{this.state.article.fields.projectLength}</p>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div className='flex h-screen'>
-//                                     <div className='flex flex-col justify-center gap-2 px-32'>
-//                                         <p className='text-[#8A8A8A]'>Contributors</p>
-//                                         <div dangerouslySetInnerHTML={{ __html: marked(this.state.article.fields.contributors) }} className="underline-animation bg-gradient-to-r from-cyan-500 to-blue-500 text-[25px] tracking-wider" />
-//                                     </div>
-//                                 </div>
-//                                 <div className='flex h-screen'>
-//                                     <div className='flex justify-between items-center px-32 w-3/4'>
-//                                         <div className='flex flex-col gap-2'>
-//                                             <p className='text-[#8A8A8A]'>Posted date</p>
-//                                             <p className='underline-animation bg-gradient-to-r from-cyan-500 to-blue-500 text-[25px] tracking-wider'>{moment(this.state.article.sys.updatedAt).format('ll')}</p>
-//                                         </div>
-//                                         <div className='flex flex-col gap-2'>
-//                                             <p className='text-[#8A8A8A]'>Posted by</p>
-//                                             <p className='underline-animation bg-gradient-to-r from-cyan-500 to-blue-500 text-[25px] tracking-wider'>{this.state.article.fields.postedBy}</p>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-// 
