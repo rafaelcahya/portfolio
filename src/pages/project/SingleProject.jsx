51f1040cda0094171cf6c7e730b6fea2c5d15e05
rafaelcahya <cahyaputraugira95@gmail.com>
2022-07-09 11:15:15 +0700

    Update

--- src/pages/project/SingleProject.jsx
+++ src/pages/project/SingleProject.jsx
@@ -1,4 +1,3 @@
-import { marked } from 'marked';
 import React, { Component } from 'react';
 import client from '../../client';
 
@@ -29,14 +28,17 @@ export default class SingleProject extends Component {
             !this.state.article ? <Loading/> : (
                 <>
                     <Navbar/>
-                    <div className='bg-[#eceef0] text-[#48484a] dark:text-[#d0d0d0] dark:bg-[#10101A] duration-200 jakarta tracking-wider'>
+                    <div className='bg-[#eceef0] text-[#48484a] dark:text-[#f4f4f4] dark:bg-[#10101A] duration-200 jakarta tracking-wider'>
                         <div className='flex h-screen'>
                             <div className='m-auto flex flex-col gap-8 text-center'>
                                 <a href={this.state.article.fields.hyperlink} target='_blank' rel="noreferrer" className='jakarta-700 text-[48px]' data-aos='fade-up' data-aos-easing="ease-out" data-aos-duration="500">{this.state.article.fields.title}</a>
                                 <p className='text-[20px] text-[#8A8A8A]' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500">{this.state.article.fields.outline}</p>
+                                <div>
+                                    <p className='text-[20px]' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500">{this.state.article.fields.type}</p>
+                                </div>
                             </div>
                         </div>
-                        <div className='flex flex-col gap-96 py-32 px-5'>
+                        <div className='flex flex-col gap-72 py-32 px-5'>
                             <div className='flex flex-col gap-10 mx-10 md:mx-32 xl:mx-64'>
                                 <p className='jakarta-700 text-[25px]' data-aos='fade-up' data-aos-easing="ease-out" data-aos-duration="500">{this.state.article.fields.title}</p>
                                 <p className='text-[18px] lg:text-[22px] w-full lg:w-4/5 leading-9' data-aos='fade-up' data-aos-easing="ease-out" data-aos-delay="100" data-aos-duration="500">{this.state.article.fields.description}</p>
